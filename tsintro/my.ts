856420f59e53e3b512ad64e0ab5ac127b07bf931
SonalMane254 <manesonal254@gmail.com>
2018-09-30 11:33:39 -0400

    add/sub

--- tsintro/my.ts
+++ tsintro/my.ts
@@ -3,3 +3,18 @@ var x , y, z;
 	y = 6;
 	z = x + y;
 	console.log('sum is '+z)
+
+	x = 5;
+	y = 2;
+	z = x - y;
+	console.log('sub is '+z)
+
+	x = 5;
+	y = 2;
+	z = x / y;
+	console.log('sub is '+z)
+
+	x = 5;
+	y = 2;
+	z = x * y;
+	console.log('sub is '+z)
